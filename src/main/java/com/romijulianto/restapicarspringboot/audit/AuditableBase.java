3e4517b03b35e7d7d6d646efe3ac5b40f11351c6
Romi Julianto <romyjulians@gmail.com>
2022-09-28 10:21:16 +0700

    add AuditableBase for tracking Audit

---
+++ src/main/java/com/romijulianto/restapicarspringboot/audit/AuditableBase.java
@@ -0,0 +1,40 @@
+package com.romijulianto.restapicarspringboot.audit;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AuditableBase {
+
+    /* add property/attribute table */
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+    @LastModifiedDate
+    private Date modifiedAt;
+
+    /* add Getter and Setter */
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(Date modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+}
